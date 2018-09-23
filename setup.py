e3e49355180c7615b5c86387c5f69869c62c5434
Matteo Nastasi <nastasi@alternativeoutput.it>
2018-09-23 13:44:36 +0200

    proper setup() arguments

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="enjoy",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Matteo Nastasi",
+    author_email="nastasi@alternativeoutput.it",
+    description="Backend for full interactive web-app",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alternativeoutput/enjoy",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
